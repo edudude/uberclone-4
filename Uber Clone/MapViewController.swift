89454b07cd83d7f4ba97743bd60de2b70a087d69
Abhishek Meher <devaskbiz@gmail.com>
2018-03-03 11:49:20 +0530

    added animate camera to current location and current location button.

--- Uber Clone/MapViewController.swift
+++ Uber Clone/MapViewController.swift
@@ -8,26 +8,25 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
 class MapViewController: UIViewController {
 
+
+    var locationManager : CLLocationManager = CLLocationManager()
+    var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let googlemapsapikey = "AIzaSyA2WLvreHIIAiFnejU3gJiuI_mkzn4kjpw"
         navigationItem.title = "Hello Map"
         GMSServices.provideAPIKey(googlemapsapikey)
-        let camera = GMSCameraPosition.camera(withLatitude: -33.868,
-                                              longitude: 151.2086,
-                                              zoom: 14)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
-        let marker = GMSMarker()
-        marker.position = camera.target
-        marker.snippet = "Hello World"
-        //marker.appearAnimation = kGMSMarkerAnimationPop()
-        marker.map = mapView
-        
-        view = mapView    }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -35,6 +34,8 @@ class MapViewController: UIViewController {
     }
     
 
+    
+    
     /*
     // MARK: - Navigation
 
@@ -45,4 +46,42 @@ class MapViewController: UIViewController {
     }
     */
 
+}
+
+
+extension MapViewController:GMSMapViewDelegate {
+    func mapViewSnapshotReady(_ mapView: GMSMapView) {
+        mapView.isHidden = false
+    }
+}
+
+extension MapViewController:CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation: CLLocation = locations[locations.count - 1]
+        print(lastLocation.coordinate.latitude)
+        print(lastLocation.coordinate.longitude)
+        
+        let camera = GMSCameraPosition.camera(withLatitude:lastLocation.coordinate.latitude,
+                                              longitude: lastLocation.coordinate.longitude,
+                                              zoom: 14)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            self.mapView = GMSMapView.map(withFrame:  self.view.frame, camera: camera)
+            self.mapView.settings.compassButton = true
+            self.mapView.isMyLocationEnabled = true
+            self.mapView.settings.myLocationButton = true
+            self.mapView.delegate = self
+            
+            let marker = GMSMarker()
+            marker.position = camera.target
+            marker.snippet = "Hello World"
+            marker.appearAnimation = GMSMarkerAnimation.pop
+            marker.map = self.mapView
+            
+            //only to unhide it when map is completely loaded
+            self.mapView.isHidden = true
+            self.view.addSubview(self.mapView)
+            self.locationManager.stopUpdatingLocation()
+        }
+    }
 }
